e9d6fc1742cddb919526e44f78034ce12de9fc66
Xaivntaaj Her <dn_her@yahoo.com>
2023-04-21 16:41:34 -0500

    optimize schema class for efficiency

---
+++ Product_API.sql
@@ -0,0 +1,2 @@
+SELECT * FROM products_api.products;
+
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from marshmallow import post_load, fiels
 from dotenv import load_dotenv
 from os import environ
 
